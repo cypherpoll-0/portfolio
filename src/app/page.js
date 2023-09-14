0fd9eaaf55c43986ad6cc8a7322e42330d9ac6ba
Ayush Kedia <ayushkedia1990@gmail.com>
2023-09-14 18:12:31 +0530

    another try

--- src/app/page.js
+++ src/app/page.js
@@ -10,9 +10,10 @@ export default function Home() {
 			<div className="flex flex-row items-center place-content-center gap-x-8 text-2xl">
 				<div>
 					<Image
-						src="/images/portfolioPic.JPG"
+						src="/public/images/portfolioPic.JPG"
 						width={500}
 						height={500}
+						unoptimized
 						className="border-2 bg-blend-normal rounded-full"
 					/>
 				</div>
@@ -32,33 +33,37 @@ export default function Home() {
 					<div className="flex flex-row gap-x-4">
 						<Link href="https://www.linkedin.com/in/cypherpoll0/">
 							<Image
-								src="/images/linkedin.PNG"
+								src="/public/images/linkedin.PNG"
 								width={50}
 								height={50}
+								unoptimized
 								alt={"linkedin logo"}
 							/>
 						</Link>
 						<Link href="https://github.com/cypherpoll-0">
 							<Image
-								src="/images/github.PNG"
+								src="/public/images/github.PNG"
 								width={50}
 								height={50}
+								unoptimized
 								alt={"github logo"}
 							/>
 						</Link>
 						<Link href="https://leetcode.com/ayushkedia1990">
 							<Image
-								src="/images/leetcode.PNG"
+								src="/public/images/leetcode.PNG"
 								width={50}
 								height={50}
+								unoptimized
 								alt={"leetcode logo"}
 							/>
 						</Link>
 						<Link href="https://www.instagram.com/the_d.i.s.t.r.a.c.t.e.d_one/">
 							<Image
-								src="/images/instagram.PNG"
+								src="/public/images/instagram.PNG"
 								width={50}
 								height={50}
+								unoptimized
 								alt={"instagram logo"}
 							/>
 						</Link>
